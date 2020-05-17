39adaa04282f520e509086cad0aa4c0e8c5fea09
huongnguyen <huonghuonglacey1020@gmail.com>
2020-05-17 05:06:36 -0700

    Updates

---
+++ precache-manifest.8f4b8154fa0775226cfbcff962ca3f82.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17776b8487425f03dd86eeded4eca0a9",
+    "url": "/react_P/index.html"
+  },
+  {
+    "revision": "d4c679b633e31b9b6ed8",
+    "url": "/react_P/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cd96c3d47bb559feceb3",
+    "url": "/react_P/static/css/main.4365e67e.chunk.css"
+  },
+  {
+    "revision": "d4c679b633e31b9b6ed8",
+    "url": "/react_P/static/js/2.4649602c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_P/static/js/2.4649602c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd96c3d47bb559feceb3",
+    "url": "/react_P/static/js/main.5eac77d8.chunk.js"
+  },
+  {
+    "revision": "6210161c3c439465984d",
+    "url": "/react_P/static/js/runtime-main.eda4ee59.js"
+  },
+  {
+    "revision": "ec250e4475dda0fc23d29e4809849e1d",
+    "url": "/react_P/static/media/background.ec250e44.jpg"
+  },
+  {
+    "revision": "609f6f4b5ee207f1d9b1cebef2f73fab",
+    "url": "/react_P/static/media/burgerImage.609f6f4b.jpg"
+  },
+  {
+    "revision": "07c6db762204fdf477fd0407f862eb8e",
+    "url": "/react_P/static/media/hw10.07c6db76.jpg"
+  },
+  {
+    "revision": "131d3fcd0a5e2f8939d1d6c4e3420b57",
+    "url": "/react_P/static/media/noteImage.131d3fcd.jpg"
+  },
+  {
+    "revision": "46a0ed096fd0b6548bd87c817f24f7d8",
+    "url": "/react_P/static/media/profile.46a0ed09.jpg"
+  },
+  {
+    "revision": "d886d44d847a7347da9880e77b388d8a",
+    "url": "/react_P/static/media/pwImage.d886d44d.jpg"
+  },
+  {
+    "revision": "142f1e3f558769b0edcb107308299fc1",
+    "url": "/react_P/static/media/resume.142f1e3f.jpg"
+  },
+  {
+    "revision": "72f86050923e7c3b577a86c2c9451cdc",
+    "url": "/react_P/static/media/trackerImage.72f86050.jpg"
+  },
+  {
+    "revision": "2d0bdc0da223bd65e2d8bc452ce17b72",
+    "url": "/react_P/static/media/weatherImage.2d0bdc0d.jpg"
+  }
+]);
